4d62e0d37859142496c504dd8e02c2621b6ef28e
Mohamed Alkhatib <m.abdelalim2017@gmail.com>
2026-02-07 13:35:00 +0300

    Update Lesson.cs

--- Lesson.cs
+++ Lesson.cs
@@ -6,6 +6,9 @@
         public string Title { get; set; } = string.Empty;
         public string? Content { get; set; }
 
+        // Added: video for each lesson
+        public string? VideoUrl { get; set; }
+
         public int SubjectId { get; set; }
         public Subject Subject { get; set; }
     }
